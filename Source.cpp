c4852cac341c1ead329c339b46e4aff4747b4eae
Ahmed-01116 <m.ahmed.01116@gmail.com>
2026-04-21 22:06:02 +0500

    hello
    hello

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+int main() {
+    // SFML 3 uses sf::VideoMode({width, height})
+    auto window = sf::RenderWindow(sf::VideoMode({ 800, 600 }), "SFML 3 + vcpkg Success!");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Cyan);
+    shape.setOrigin({ 100.f, 100.f }); // Center the circle
+    shape.setPosition({ 400.f, 300.f });
+
+    while (window.isOpen()) {
+        // SFML 3 new event polling style
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
